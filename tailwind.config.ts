99b7ed1aede1b846f29766b21073c432d4015173
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2024-12-06 21:08:22 +0530

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,14 +5,10 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./util/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 } satisfies Config;
